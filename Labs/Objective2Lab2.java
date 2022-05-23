6ef4656d4fa51739fede95898185cb0505fb9187
Steph Karlsen <becomingsteph@gmail.com>
2022-05-23 16:04:07 -0600

    Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2 {
+    public static void main(String[] args) {
+      String name = "Stephanie Karlsen";
+      int age = 46;
+      String state = "Michigan";
+
+      System.out.println(name);
+      System.out.println(age);
+      System.out.println(state);
+      
+    }
+}
