e3f9201857c6ba1d4b1451e0e1e1509cbf8314d7
Steph Karlsen <becomingsteph@gmail.com>
2022-05-31 17:06:39 -0600

    Objective5Lab1

--- Objective5Lab1.java
+++ Objective5Lab1.java
@@ -6,6 +6,7 @@ public class Objective5Lab1 {
       if(num1 > num2) {
         System.out.println(num1 + " is greater than " + num2);
       }
+      
       else if(num2 > num1) {
         System.out.println(num2 + " is greater than " + num1);
       }
