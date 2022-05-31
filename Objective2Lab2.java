f9ff0c6e15f2dc576e9b88637fc5ad82c5da038e
Steph Karlsen <becomingsteph@gmail.com>
2022-05-31 17:17:18 -0600

    uploading prework files

---
+++ Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+    public static void main(String[] args) {
+      String name = "Stephanie Karlsen";
+      int age = 46;
+      String state = "Michigan";
+
+      System.out.println(" Name: " + name + "  Age: " + age + "  State: " + state);
+
+    }
+}
