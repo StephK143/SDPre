59912fa3d9642b74359ce617b79edee6f8e19b92
Steph Karlsen <becomingsteph@gmail.com>
2022-05-31 22:14:49 -0600

    Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,14 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int count = 0;
+    int sum = 0;
+
+
+    while (count <= 20) {
+      sum = count + sum;
+      count = count + 1;
+    }
+      System.out.println(sum);
+
+  }
+}
