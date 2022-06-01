06afaed523e5e887365a943bcc57d4f810d51edc
Steph Karlsen <becomingsteph@gmail.com>
2022-06-01 14:06:08 -0600

    Objective9Lab3

---
+++ Labs/MethodsExample.java
@@ -0,0 +1,27 @@
+public class MethodsExample {
+  public static void main(String[] args) {
+
+    printSquare();
+    System.out.println();
+    printRectangle();
+  }
+
+  public static void printSquare() {
+    int counter = 0;
+    int sum = 0;
+
+    while (counter <=10) {
+      sum = counter + sum;
+      counter = counter + 1;
+    System.out.println("XXXXXXXXXXXXXXXXXXXX");
+  }
+  }
+
+  public static void printRectangle() {
+
+    for (int counter = 1; counter <= 20; counter++) {
+      System.out.println("XXXXXXXXXXXXXXXXXXXX");
+    }
+
+  }
+}
